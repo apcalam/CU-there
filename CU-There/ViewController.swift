fd9419151ff24a21c0e7a16254700ec128ea39fb
Ashrita Raman <ashritaraman@dhcp-ccc-4572.eduroam.cornell.edu>
2018-04-27 20:53:32 -0400

    Add xcode project

---
+++ CU-There/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CU-There
+//
+//  Created by Ashrita Raman on 4/27/18.
+//  Copyright © 2018 Ashrita Raman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
